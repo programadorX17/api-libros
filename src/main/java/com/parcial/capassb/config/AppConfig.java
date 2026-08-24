41d3d5b381b086c0cdfa98bda9da641ade0f3d4d
Andres Suarez <realbarcelona339@gmail.com>
2026-08-24 14:34:45 -0500

    Agregando bean de ModelMapper en AppConfig

--- src/main/java/com/parcial/capassb/config/AppConfig.java
+++ src/main/java/com/parcial/capassb/config/AppConfig.java
@@ -1,8 +1,15 @@
 package com.parcial.capassb.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 
 }
